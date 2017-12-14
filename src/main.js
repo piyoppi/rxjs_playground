bbacdae937918ccbbe8a312ad82d9757bf64ab3a
piyoppi <3podvator@gmail.com>
2017-12-14 11:16:41 +0900

    scanとかintervalとか試してみた

--- src/main.js
+++ src/main.js
@@ -1,6 +1,46 @@
 import Rx from 'rxjs/Rx';
 
-//of
-Rx.Observable
-    .of(1, 2, 3)
-    .subscribe( val => console.log(val) );
+window.onload = function() {
+
+    Rx.Observable
+        .of(1, 2, 3)
+        .map( val => val * 10 )
+        .mapTo(23)
+        .subscribe( val => console.log(val) );
+
+    //-------------------------------------------------------------------------
+
+    Rx.Observable
+        .fromEvent( document.querySelector('#test1'), 'click')
+        .mapTo(3)
+        .map( val => val * 10 )
+        .subscribe( val => document.getElementById("result1").innerText = val );
+
+    //-------------------------------------------------------------------------
+    
+    Rx.Observable
+        .fromEvent( document.querySelector('#test2'), 'click')
+        .mapTo(3)
+        .scan( (acc, one) => acc + one, 0 )
+        .subscribe( val => document.getElementById("result2").innerText = val );
+
+    //-------------------------------------------------------------------------
+    
+    Rx.Observable
+        .fromEvent( document.querySelector('#test3'), 'click')
+        .mapTo(3)
+        .scan( (acc, one) => acc + one, 0 )
+        .filter( val => val % 9 === 0 )
+        .subscribe( val => document.getElementById("result3").innerText = val );
+
+    //-------------------------------------------------------------------------
+
+    Rx.Observable.interval(1000)
+        .take(10)
+        .mapTo('■')
+        .scan( (acc, one) => `${acc}${one}`, '' )
+        .subscribe( val => document.getElementById("result4").innerText = val,
+            (err) => console.log(err),
+            ()    => document.getElementById("result4").innerText = 'complete'
+        );
+}
